53598708b9de8e5d34ef09ace2b998c661392bc1
N <noel.mcloughlin@gmail.com>
2019-03-26 00:40:33 +0000

    Set package desc field

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ async function check(objects) {
     }
 }
 
-const Dotenv     = require('dotenv').config();
+const Dotenv     = require('dotenv')
+const Result     = Dotenv.config();
 const Hapi       = require('hapi');
 const HapiCookie = require('hapi-auth-cookie');
 const Vision     = require('vision');
@@ -20,13 +21,16 @@ const server     = Hapi.server({ port: process.env.PORT || 3000, });
 require('./app/models/db');
 
 // Check we are okay
-check([Dotenv, Hapi, HapiCookie, Vision, Inert, Nunjucks, Routes, server])
+check([Result, Hapi, HapiCookie, Vision, Inert, Nunjucks, Routes, server])
 
 
 async function provision() {
-  await server.register(Inert);
-  await server.register(Vision);
-  await server.register(HapiCookie);
+  //#await server.register(Inert);
+  //#await server.register(Vision);
+  //#await server.register(HapiCookie);
+  await server.register(require('inert'));
+  await server.register(require('vision'));
+  await server.register(require('hapi-auth-cookie'));
 
   server.views({
     engines: { // Vision Templates rendering support for Hapi: https://github.com/hapijs/vision#nunjucks
