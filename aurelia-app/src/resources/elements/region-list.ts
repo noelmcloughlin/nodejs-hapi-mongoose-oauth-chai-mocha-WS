876c3d05ac7636930e9247be5f9d1b2b49c7bba4
N <noel.mcloughlin@gmail.com>
2019-05-30 08:16:02 +0100

    Split Typescript Aurelia to own Repo

--- aurelia-app/src/resources/elements/region-list.ts
+++
@@ -1,7 +0,0 @@
-import { bindable } from 'aurelia-framework';
-import { Region } from '../../services/poi-types';
-
-export class RegionList {
-  @bindable
-  regions: Region[];
-}
--- aurelia-app/src/services/messages.ts
+++
@@ -1,6 +0,0 @@
-export class TotalPoi {
-  total: number;
-  constructor(total: number) {
-    this.total = total;
-  }
-}
